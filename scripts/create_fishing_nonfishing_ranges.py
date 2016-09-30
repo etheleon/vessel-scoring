452998028d78729b8239d9af37c65af8c1520eb6
bitsofbits <tim@bitsofbits.com>
2016-09-30 09:34:40 -0700

    code to create range based description of fishing / non-fishing, plus basic test

---
+++ scripts/create_fishing_nonfishing_ranges.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import pandas as pd
+import datetime
+import logging
+
+def get_kristina_timestamp(x):
+    if 'DATETIME' in x:
+        return datetime.datetime.strptime(x['DATETIME'], "%Y-%m-%d %H:%M:%S")
+    elif 'TIME' in x:
+        return datetime.datetime.strptime(x['TIME'], "%Y%m%d_%H%M%S")
+    else:
+        assert False, "NO TIME: {}".format(x)
+
+def get_kristina_is_fishing(x):
+	if 'COARSE_FIS' in x:
+		return x['COARSE_FIS']
+	else:
+		return x['COARSE-FIS']
+
+
+
+kristina = {'mmsi' : lambda x: x['MMSI'],
+            'timestamp' : get_kristina_timestamp,
+            'is_fishing' : get_kristina_is_fishing}
+
+
+ 
+def points_from_path(pth, dialect=kristina):
+	df = pd.read_csv(pth)
+	results = []
+	get_mmsi = dialect['mmsi']
+	get_time = dialect['timestamp']
+	get_fish = dialect['is_fishing']
+	for index, row in df.iterrows():
+		results.append([get_mmsi(row), get_time(row), get_fish(row)])
+	return results
+
+
+def dedup_and_sort_points(points):
+	points.sort()
+	dedupped = []
+	last_key = (None, None)
+	last_fishing = None
+	for mmsi, timestamp, is_fishing in points:
+		key = (mmsi, timestamp)
+		if key == last_key:
+			if is_fishing != last_fishing:
+				dedupped[-1] = (mmsi, timestamp, None)
+		else:
+			dedupped.append((mmsi, timestamp, is_fishing))
+			last_key = key
+			last_fishing = is_fishing
+	return dedupped
+
+
+def ranges_from_path(pth, dialect=kristina):
+	points = dedup_and_sort_points(points_from_path(pth))
+	current_state = None
+	current_mmsi = None
+	ranges = []
+	for mmsi, time, state in points:
+		if mmsi != current_mmsi or state != current_state:
+			if current_state is not None:
+				ranges.append((mmsi, range_start.isoformat(), last_time.isoformat(), current_state))
+			current_state = state
+			range_start = time
+			current_mmsi = mmsi
+		last_time = time
+	if current_state is not None:
+		ranges.append((mmsi, range_start.isoformat(), last_time.isoformat(), current_state))
+	return ranges
+
+
+def ranges_from_multiple_paths(paths):
+	results = []
+	for pth in paths:
+		logging.info("converting {}".format(pth))
+		try:
+			results.extend(ranges_from_path(pth))
+		except StandardError as err:
+			logging.warning("conversion failed for {}".format(pth))
+			logging.warning(repr(err))
+	return results
+
+
+
+if __name__ == "__main__":
+	import argparse
+	import glob
+	import os
+	parser = argparse.ArgumentParser(description="extract fishing/nonfishing ranges from Kristina's data")
+	parser.add_argument('--source-dir', help='directory holding sources to convert')
+	parser.add_argument('--dest-path', help='path to write results to')
+	args = parser.parse_args()
+	in_paths = glob.glob(os.path.join(args.source_dir, "*.csv"))
+	results = ranges_from_multiple_paths(in_paths)
+	with open(args.dest_path, "w") as f:
+		f.write("mmsi,start_time,end_time,is_fishing\n")
+		for row in results:
+			f.write("{}\n".format(','.join(str(x) for x in row)))
---
+++ scripts/test_create_ranges.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from __future__ import division
+import numpy as np
+import pandas as pd
+import datetime
+import logging
+import create_fishing_nonfishing_ranges as cfr
+
+def test_round_trip(source_paths, range_path):
+	ranges = pd.read_csv(range_path)
+	for pth in in_paths:
+		logging.info("testing file: {}".format(pth))
+		examples = pd.read_csv(pth)
+		mmsi = sorted(set(examples['MMSI']))
+		for m in mmsi:
+			logging.info("processing mmsi: {}".format(pth))
+			sub_ranges = ranges[ranges['mmsi'] == m]
+			sub_examples = examples[examples['MMSI'] == m]
+			starts = pd.to_datetime(sub_ranges['start_time'])
+			stops = pd.to_datetime(sub_ranges['end_time'])
+			is_fishing = (sub_ranges['is_fishing'] == 1)
+			logging.info("{} ranges present".format(len(starts)))
+			example_times = np.array([cfr.get_kristina_timestamp(x) for (_, x) in sub_examples.iterrows()])
+			example_fishing = np.array([cfr.get_kristina_is_fishing(x) for (_, x) in sub_examples.iterrows()], dtype=bool)
+			count = 0
+			for t0, t1, state in zip(starts, stops, is_fishing):
+				mask = (example_times >= t0) & (example_times <= t1)
+				if not np.alltrue(example_fishing[mask] == state):
+					logging.error("item did not round trip: {} {} {}".format(mmsi, t0, t1, state))
+					logging.error(pth)
+				count += mask.sum()
+			if count != len(sub_examples):
+				logging.warning("{}% of samples covered for {}: {}".format(100 * count / len(sub_examples), pth, m))
+
+
+if __name__ == "__main__":
+	import argparse
+	import glob
+	import os
+	logging.getLogger().setLevel("WARNING")
+	parser = argparse.ArgumentParser(description="extract fishing/nonfishing ranges from Kristina's data")
+	parser.add_argument('--source-dir', help='directory where converted sources were drawn from')
+	parser.add_argument('--range-path', help='path to range file')
+	args = parser.parse_args()
+	in_paths = glob.glob(os.path.join(args.source_dir, "*.csv"))
+	test_round_trip(in_paths, args.range_path)
